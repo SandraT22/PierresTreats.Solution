64588bc16b17e79f476485e28861799995faee65
Sandra Tena <sandratena06@gmail.com>
2022-08-12 13:34:20 -0700

    add missing using directives in treats controller

--- PierresTreat/Controllers/TreatsController.cs
+++ PierresTreat/Controllers/TreatsController.cs
@@ -1,9 +1,13 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
-using PierresTreat.Models;
+using Library.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace PierresTreat.Controllers
 {
