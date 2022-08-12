9e16bd25c5921c2953fbc7801db23d8cae831bd0
Sandra Tena <sandratena06@gmail.com>
2022-08-12 13:33:21 -0700

    fix typos in treat controller

--- PierresTreat/Controllers/TreatsController.cs
+++ PierresTreat/Controllers/TreatsController.cs
@@ -35,21 +35,21 @@ namespace PierresTreat.Controllers
 
     public ActionResult Create()
     {
-      ViewBag.AuthorId = new SelectList(_db.Authors, "AuthorId", "Name");
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
       return View();
     }
 
     [HttpPost]
-    public async Task<ActionResult> Create(Treat treat, int AuthorId)
+    public async Task<ActionResult> Create(Treat treat, int FlavorId)
     {
       var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
       var currentUser = await _userManager.FindByIdAsync(userId);
       treat.User = currentUser;
       _db.Treats.Add(treat);
       _db.SaveChanges();
-      if (AuthorId != 0)
+      if (FlavorId != 0)
       {
-        _db.AuthorTreat.Add(new AuthorTreat() { AuthorId = AuthorId, TreatId = treat.TreatId });
+        _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
       }
       _db.SaveChanges();
       return RedirectToAction("Index");
@@ -59,7 +59,7 @@ namespace PierresTreat.Controllers
     {
       var thisTreat = _db.Treats
           .Include(treat => treat.JoinEntities)
-          .ThenInclude(join => join.Author)
+          .ThenInclude(join => join.Flavor)
           .FirstOrDefault(treat => treat.TreatId == id);
       // var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
       ViewBag.Copies = _db.Copies.Where(copy => copy.TreatId == id);
@@ -69,35 +69,35 @@ namespace PierresTreat.Controllers
     public ActionResult Edit(int id)
     {
       var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
-      ViewBag.AuthorId = new SelectList(_db.Authors, "AuthorId", "Name");
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
       return View(thisTreat);
     }
 
     [HttpPost]
-    public ActionResult Edit(Treat treat, int AuthorId)
+    public ActionResult Edit(Treat treat, int FlavorId)
     {
-      if (AuthorId != 0)
+      if (FlavorId != 0)
       {
-        _db.AuthorTreat.Add(new AuthorTreat() { AuthorId = AuthorId, TreatId = treat.TreatId });
+        _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
       }
       _db.Entry(treat).State = EntityState.Modified;
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
 
-    public ActionResult AddAuthor(int id)
+    public ActionResult AddFlavor(int id)
     {
       var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
-      ViewBag.AuthorId = new SelectList(_db.Authors, "AuthorId", "Name");
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
       return View(thisTreat);
     }
 
     [HttpPost]
-    public ActionResult AddAuthor(Treat treat, int AuthorId)
+    public ActionResult AddFlavor(Treat treat, int FlavorId)
     {
-      if (AuthorId != 0)
+      if (FlavorId != 0)
       {
-        _db.AuthorTreat.Add(new AuthorTreat() { AuthorId = AuthorId, TreatId = treat.TreatId });
+        _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
       }
       _db.SaveChanges();
       return RedirectToAction("Index");
@@ -119,10 +119,10 @@ namespace PierresTreat.Controllers
     }
 
     [HttpPost]
-    public ActionResult DeleteAuthor(int joinId)
+    public ActionResult DeleteFlavor(int joinId)
     {
-      var joinEntry = _db.AuthorTreat.FirstOrDefault(entry => entry.AuthorTreatId == joinId);
-      _db.AuthorTreat.Remove(joinEntry);
+      var joinEntry = _db.FlavorTreat.FirstOrDefault(entry => entry.FlavorTreatId == joinId);
+      _db.FlavorTreat.Remove(joinEntry);
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
