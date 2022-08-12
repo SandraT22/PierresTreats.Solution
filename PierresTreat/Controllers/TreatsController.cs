85594a92ba5841022a0c74d79bd32577879f9b12
Sandra Tena <sandratena06@gmail.com>
2022-08-12 14:31:11 -0700

    finish controllers and gert started on adding roles

--- PierresTreat/Controllers/TreatsController.cs
+++ PierresTreat/Controllers/TreatsController.cs
@@ -1,10 +1,10 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
-using Library.Models;
+using PierresTreat.Models;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Flavorization;
 using Microsoft.AspNetCore.Identity;
 using System.Threading.Tasks;
 using System.Security.Claims;
